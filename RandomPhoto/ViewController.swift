f27505813201b75689542e2044b86a02744e5bee
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-12 10:14:42 +0800

    first commit

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+            return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemPink
         // Do any additional setup after loading the view.
     }
 
